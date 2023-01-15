8aedc645a7e5c8d7ae1adf41c8f74d328bac15d3
Ikromjon <ikromjon98.98@icloud.com>
2023-01-16 00:00:42 +0100

    Installed dependencies and connected to the mongodb

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.set("strictQuery", false);
+mongoose.connect(uri, () => {
+    console.log("Connected to MongoDB");
+});/*  */
+const connection = mongoose.connection;
+console.log(connection.id);
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
